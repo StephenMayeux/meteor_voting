0c52b5077bc0519de8a0b0b5f1c6e3f019c06c8c
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-23 16:24:50 +0900

    create routes

--- server/main.js
+++ server/main.js
@@ -1,5 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { Polls } from '../imports/collections/polls';
 
 Meteor.startup(() => {
   // add publications here
+  
+  Meteor.publish('allPolls', function() {
+    return Polls.find({});
+  });
+
+  Meteor.publish('userPolls', function() {
+    return Polls.find({ ownerId: this.userId });
+  });
 });
